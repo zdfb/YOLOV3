3ddd76e3357c3b89074574904c935934a7cfdbc0
unknown <951622256@qq.com>
2021-10-23 19:49:55 +0800

    Initial commit

--- .history/predict_images_20211023185148.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-from utils.utils_yolo import YOLO
-
-yolo = YOLO()
-image = Image.open('bird1.jpg')
-image = yolo.detect_image(image)
-image.show()
--- .history/predict_images_20211023185746.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-from utils.utils_yolo import YOLO
-
-yolo = YOLO()
-image = Image.open('street.jpg')
-image = yolo.detect_image(image)
-image.show()
