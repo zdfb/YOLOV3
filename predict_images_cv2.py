8fa02108fa1cdee1354ccb4450d6b95205354ead
zdfb <41527982+zdfb@users.noreply.github.com>
2021-10-29 17:00:31 +0800

    update 10.29

--- predict_images_cv2.py
+++ predict_images_cv2.py
@@ -4,6 +4,7 @@ from utils_cv2.utils_yolo import YOLO
 image_path = 'Image_samples/street.jpg'  # 测试图片路径
 
 yolo = YOLO()
+
 image = Image.open(image_path)
 image = yolo.detect_image(image)
 image.show()
